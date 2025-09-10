01afff101c1b85ad47e59192b626772a1f4c557c
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-10 13:48:53 +0800

    Created tasksheet-1-1-3 and added needed variables

---
+++ tasksheet-1-1-3.java
@@ -0,0 +1,10 @@
+//Title: Writing Control flow statements using Loops
+
+class evenOdd{
+    public static void main(String[] args) {
+        //variable creation
+       int check_number = 10;
+       String message;
+
+    }
+}
