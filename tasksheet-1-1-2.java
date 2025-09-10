29286f9e8dffad5f6e0126d6c326287c09f9229c
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-10 13:25:37 +0800

    Cleaned up comments

--- tasksheet-1-1-2.java
+++ tasksheet-1-1-2.java
@@ -7,22 +7,17 @@ class ArithmeticTask {
         int result = 1 + 2; // result is now 3
         System.out.println(result);
 
-//        result = result - 1; // result is now 2
-        result -= 1;    //changed to compound operation
+        result -= 1;    //changed to compound operation, result is now 2
         System.out.println(result);
 
-//        result = result * 2; // result is now 4
-        result *= 2;    //changed to compound operation
+        result *= 2;    //changed to compound operation, result is now 4
         System.out.println(result);
 
-//        result = result / 2; // result is now 2
-        result /= 2;    //changed to compound operations
+        result /= 2;    //changed to compound operations, result is now 2
         System.out.println(result);
 
-//        result = result + 8; // result is now 10
-        result += 8;
-//        result = result % 7; // result is now 3
-        result %= 7;
+        result += 8; //changed to compound operations, result is now 10
+        result %= 7; //changed to compound operations, result is now 3
         System.out.println(result);
 
     }
