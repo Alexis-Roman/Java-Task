e90e6f63c157e8b537704e58cd995f4892a65749
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-10 13:50:06 +0800

    Created a for loop that will display if num is even or odd

--- tasksheet-1-1-3.java
+++ tasksheet-1-1-3.java
@@ -6,5 +6,11 @@ class evenOdd{
        int check_number = 10;
        String message;
 
+        //creating the loop
+        for (int i = 1 ; i <= check_number ; i++){
+            message = (i%2==0)? i+ " is even number" : i+ " is odd number";
+            System.out.println(message);
+        }
+
     }
 }
