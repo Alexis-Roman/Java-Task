f47a2c536018a19b8aabc935b13d6e9332c12615
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-11 09:51:38 +0800

    Tested each boolean condition to see if it will output true

--- task-3.java
+++ task-3.java
@@ -3,16 +3,19 @@ class Task3{
     public static void main(String[] args) {
 
         String a = new String("Wow");
-        String b = "Wow";
+        String b = a;
         String c = "a";
-        String d = "c";
+        String d = "Wow!";
 
         boolean b1 = a == b;
+        System.out.println("b1 is "+ b1);
         boolean b2 = d.equals(b+ "!");
+        System.out.println("b2 is "+ b2);
         boolean b3 = !c.equals(a);
+        System.out.println("b3 is "+ b3);
 
         if (b1 && b2 && b3){
             System.out.println("Success!");
-        }
+        }else System.out.println("failed");
     }
 }
