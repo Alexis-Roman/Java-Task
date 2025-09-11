91a9df9caa4c79ae1211c9a93f78e614063d98e3
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-11 09:53:06 +0800

    Removed unnecessary souts, each if statement resolves as true

--- task-3.java
+++ task-3.java
@@ -8,14 +8,11 @@ class Task3{
         String d = "Wow!";
 
         boolean b1 = a == b;
-        System.out.println("b1 is "+ b1);
         boolean b2 = d.equals(b+ "!");
-        System.out.println("b2 is "+ b2);
         boolean b3 = !c.equals(a);
-        System.out.println("b3 is "+ b3);
 
         if (b1 && b2 && b3){
             System.out.println("Success!");
-        }else System.out.println("failed");
+        }
     }
 }
