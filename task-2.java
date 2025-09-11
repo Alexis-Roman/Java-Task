bc0b551b275f8991af2958bc042738fea09a411f
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-11 09:34:51 +0800

    Created task 2 and added all primitives

---
+++ task-2.java
@@ -0,0 +1,22 @@
+//Create all of the primitives (except long and double) with
+//different values. Concatenate them into a string and
+//print it to the screen so it will print: H3110 world 2.0 true
+class primitiveConcatenating{
+    public static void main(String[] args) {
+        // creating all primitives
+
+        byte b = 3;
+        short s = 1;
+        int i = 0;
+        float f = 2.0f;
+        char H = 'H';
+        char w = 'w';
+        char o = 'o';
+        char r = 'r';
+        char l = 'l';
+        char d = 'd';
+        char space = ' ';
+        boolean bool = true;
+
+    }
+}
--- tasksheet-1-1-3.java
+++ tasksheet-1-1-3.java
@@ -10,6 +10,7 @@ class evenOdd{
         for (int i = 1 ; i <= check_number ; i++){
             message = (i%2==0)? i+ " is even number" : i+ " is odd number";
             System.out.println(message);
+
         }
 
     }
