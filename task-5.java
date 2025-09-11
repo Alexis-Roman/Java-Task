1ab98edaacba8a415dafc277a40c0b91e388623a
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-11 10:34:18 +0800

    Created Task 5 an added a scanner class

---
+++ task-5.java
@@ -0,0 +1,15 @@
+// Largest Number Checker
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class NumChecker{
+    public static void main(String[] args) {
+        //accepts user input
+        Scanner inputNum = new Scanner(System.in);
+        int[] numbers = new int[3];
+
+        System.out.println("*LARGEST NUMBER CHECKER*");
+
+    }
+}
