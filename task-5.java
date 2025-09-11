b703eb6809fd510d0e1987c2653e4d0d621a3271
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-11 10:48:52 +0800

    Sorted the array and print if numbers are equal or not. Used try-catch for exeption handling

--- task-5.java
+++ task-5.java
@@ -1,6 +1,7 @@
 // Largest Number Checker
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class NumChecker{
@@ -9,18 +10,25 @@ class NumChecker{
         Scanner inputNum = new Scanner(System.in);
         int[] numbers = new int[3];
 
-        System.out.println("*LARGEST NUMBER CHECKER*");
-        System.out.print("Enter your first number: ");
-        numbers[0] = inputNum.nextInt();
+        try {
+            System.out.println("*LARGEST NUMBER CHECKER*");
+            System.out.print("Enter your first number: ");
+            numbers[0] = inputNum.nextInt();
 
-        System.out.print("Enter your second number: ");
-        numbers[1] = inputNum.nextInt();
+            System.out.print("Enter your second number: ");
+            numbers[1] = inputNum.nextInt();
 
-        System.out.print("Enter your second number: ");
-        numbers[2] = inputNum.nextInt();
+            System.out.print("Enter your second number: ");
+            numbers[2] = inputNum.nextInt();
 
-        System.out.println(Arrays.toString(numbers));
-
+            //sorts the array
+            Arrays.sort(numbers);
+            System.out.println(numbers[0] == numbers[2]
+                    ? "All numbers are equal"
+                    : "The numbers " + Arrays.toString(numbers) + " are not equal");
+        }catch (InputMismatchException e){
+            System.out.println("Invalid input! Please enter a valid number.");
+        }
 
     }
 }
