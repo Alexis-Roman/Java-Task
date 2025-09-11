3e64dd4e99240ffaa7ced659584b5f03bf23af32
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-11 10:20:21 +0800

    Made a ternary operator to check if the input word is a palindrome

--- task-4.java
+++ task-4.java
@@ -7,10 +7,24 @@ class palindromeChecker{
         //accepts user input
         Scanner input = new Scanner(System.in);
         String word;
+        String reversed;
+        boolean isEqual;
 
         System.out.println("*PALINDROME CHECKER*");
         System.out.println("Enter a word: ");
         word = input.nextLine();
-        System.out.println(word);
+
+        //Reversed string
+        reversed = new StringBuilder(word).reverse().toString();
+        System.out.println(reversed);
+
+        //Check if input string is equal to reversed string
+        isEqual = word.equals(reversed);
+
+        System.out.println(isEqual
+                ? "The input string is a palindrome."
+                : "The input string is not a palindrome.");
+        input.close();
+
     }
 }
