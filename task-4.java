71614b0acaa4adc6a68aba139ab9771bb0613734
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-11 10:04:02 +0800

    Created Task 4 and added scanner class to read user input

---
+++ task-4.java
@@ -0,0 +1,16 @@
+// Palindrome Checker
+
+import java.util.Scanner;
+
+class palindromeChecker{
+    public static void main(String[] args) {
+        //accepts user input
+        Scanner input = new Scanner(System.in);
+        String word;
+
+        System.out.println("*PALINDROME CHECKER*");
+        System.out.println("Enter a word: ");
+        word = input.nextLine();
+        System.out.println(word);
+    }
+}
