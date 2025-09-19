a008ae0fa08d81905df071f83e80ecfe0c27486e
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-20 07:57:24 +0800

    Added operations

--- Task9.java
+++ Task9.java
@@ -3,20 +3,16 @@ import java.util.Scanner;
 
 public class Task9{
     public static int add(int a, int b) {
-        return 0;
-// Your code here
+        return addExact(a, b);
     }
     public static int subtract(int a, int b) {
-        return 0;
-// Your code here
+        return subtractExact(a, b);
     }
     public static int multiply(int a, int b) {
-        return 0;
-// Your code here
+        return multiplyExact(a, b);
     }
     public static float divide(int a, int b) {
-        return 0;
-// Your code here
+        return floorDiv(a, b);
     }
     public static void main(String[] args) {
         System.out.println("PERFORMING BASIC MATH OPERATIONS USING STATIC IMPORTS");
@@ -27,5 +23,10 @@ public class Task9{
         System.out.print("Enter second number: ");
         int secondNum = input.nextInt();
 
+        System.out.println();
+        System.out.println("Sum = " + add(firstNum, secondNum));
+        System.out.println("Difference = " + subtract(firstNum, secondNum));
+        System.out.println("Product = " + multiply(firstNum, secondNum));
+        System.out.println("Quotient = " + divide(firstNum, secondNum));
     }
 }
