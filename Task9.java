53d91121f222587e2fb574567a454579cc1dc3d0
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-20 07:52:21 +0800

    Uploaded Task9

---
+++ Task9.java
@@ -0,0 +1,22 @@
+import static java.lang.Math.*;
+public class Task9{
+    public static int add(int a, int b) {
+        return 0;
+// Your code here
+    }
+    public static int subtract(int a, int b) {
+        return 0;
+// Your code here
+    }
+    public static int multiply(int a, int b) {
+        return 0;
+// Your code here
+    }
+    public static float divide(int a, int b) {
+        return 0;
+// Your code here
+    }
+    public static void main(String[] args) {
+// Demonstrate the math operations here
+    }
+}
