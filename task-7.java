37f755706bdebc9a410230a327092ad3cef2e6bd
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-20 06:43:31 +0800

    Uploaded Task 7 and 8

---
+++ task-7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// Create a program where user inputs two numbers and perform 4 basic
+// arithmetic operations using 4 methods with return types and print the results
+class arithmeticOps{
+    public static void main(String[] args) {
+        Scanner inputNum = new Scanner(System.in);
+    }
+
+
+}
