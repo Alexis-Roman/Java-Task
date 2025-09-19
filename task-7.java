9fb799fce89a0e651d62a8f7bf05a57b72cd40a2
Alexis Roman <romanalexisnicole06@gmail.com>
2025-09-20 06:55:54 +0800

    Tested addition operation

--- task-7.java
+++ task-7.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 class arithmeticOps{
     public static void main(String[] args) {
         Scanner inputNum = new Scanner(System.in);
-
+        System.out.println("Enter two numbers to perform ADDITION, SUBTRACTION, MULTIPLICATION, AND DIVISION");
+        System.out.println();
         System.out.print("Enter first number: ");
         int firstNum = inputNum.nextInt();
 
         System.out.print("Enter second number: ");
         int secondNum = inputNum.nextInt();
+
+        System.out.println("Sum: " + add(firstNum, secondNum));
+    }
+
+    public static int add(int a, int b){
+        return a + b;
     }
 }
