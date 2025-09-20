e971a19d8a0bd443abf17ad2d96bcc7bea294625
Alexis Nicole Roman <104246596+Alexis-Roman@users.noreply.github.com>
2025-09-20 07:40:01 +0000

    Uploaded Task14

---
+++ Task14.java
@@ -0,0 +1,33 @@
+abstract class abstractClass {
+    abstract void abstractMethod();
+
+    void concreteMethod() {
+        // concrete methods are still allowed in abstract classes
+        System.out.println("This is a concrete method.");
+    }
+}
+
+class B extends abstractClass {
+    void abstractMethod() {
+        System.out.println("B's implementation of abstractMethod.");
+    }
+}
+
+class C extends abstractClass {
+    //your code goes here
+}
+
+public class Task14 {
+    public static void main(String args[]) {
+        B b = new B();
+        b.abstractMethod();
+        b.concreteMethod();
+
+        C c = new C();
+        c.abstractMethod();
+        c.concreteMethod();
+    }
+}
+
+
+
