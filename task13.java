9a1b98b51170518e79b6fb75dd70de833351eb59
Alexis Nicole Roman <104246596+Alexis-Roman@users.noreply.github.com>
2025-09-20 07:29:50 +0000

    Create Car class extends Vehicle

--- task13.java
+++ task13.java
@@ -17,3 +17,14 @@ class Vehicle{
     }
 }
 
+class Car extends Vehicle{
+    private int numberOfDoors;
+
+    public Car(String make, String model, int year,
+               int numberOfDoors){
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+}
+
