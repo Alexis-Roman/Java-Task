1d1504c09d8e646fab3709c030d1c1f7e3611087
Alexis Nicole Roman <104246596+Alexis-Roman@users.noreply.github.com>
2025-09-20 07:29:03 +0000

    Uploaded task 13, created Vehicle class

---
+++ task13.java
@@ -0,0 +1,19 @@
+public class task13 {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Vehicle{
+    protected String make;
+    protected String model;
+    protected int year;
+
+    public Vehicle(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+
